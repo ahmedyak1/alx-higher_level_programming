a1e7cd57a30aed6be65eeb8f24b9539a9c050d69
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-11 22:05:25 +0000

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Prints all item in list 
-agrs:my_list[]
-"""
+
 def print_list_integer(my_list=[]):
     for k in range(len(my_list)):
         print("{:d}".format(my_list[k]))
