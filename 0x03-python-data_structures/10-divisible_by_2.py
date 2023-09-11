8d99cafaabfe63ed68c09b2a62baec88547fa9b1
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-11 22:57:08 +0000

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    numbers_divisible_by_2 = []
+    for item in range(len(my_list)):
+        if my_list[item] % 2 != 0:
+            numbers_divisible_by_2.append(False)
+        else:
+           numbers_divisible_by_2.append(True) 
+
+    return (numbers_divisible_by_2)
