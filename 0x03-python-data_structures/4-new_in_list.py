c16693c07da7e9fa0417ee745675fc218989dfb8
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-11 22:46:12 +0000

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-    Substitute an element in a duplicated list at a particular location
-    args:my_list,idx,element
-"""
+
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= (len(my_list)):
         return (my_list)
