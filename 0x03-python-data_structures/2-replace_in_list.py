58a723fa839f09a44a755464fec3b4b523bf1050
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-11 22:41:53 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-    Replace an item of a list at a specific index
-    args:my_list,idx,element
-"""
+
 def replace_in_list(my_list, idx, element):
     if idx < len(my_list) and idx >= 0:
         my_list[idx] = element
