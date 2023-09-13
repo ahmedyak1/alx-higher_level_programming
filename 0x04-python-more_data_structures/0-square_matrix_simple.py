e728078335e1adfe013b3a05c1d1d4b582d4bb56
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-13 23:14:19 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_cop = matrix.copy()
-        matrix_cop = [[k**2 for k in row] for row in matrix]
-    return (matrix_cop)
+    matrixnew = matrix.copy()
+    for k in range(len(matrix)):
+        matrixnew[k] = list(map(lambda m: m**2, matrix[k]))
+    return (matrixnew)
 
