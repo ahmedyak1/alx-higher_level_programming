8c20452c8222ce3cdd896a7c2225edf0b5db9ae4
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-13 22:14:56 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import sys
+arguments_len = len(sys.argv) - 1
+
 if name == "_main_":
-    import sys
-    arguments_len = len(sys.argv) - 1
+    
     if arguments_len == 0:
         print("0 arguments.")
     elif arguments_len == 1:
@@ -9,4 +11,4 @@ if name == "_main_":
     else:
         print("{} arguments:".format(arguments_len))
     for k in range(arguments_len):
-        print("{}: {}".format(k + 1, sys.argv[k + 1])
+        print("{}: {}".format(k + 1, sys.argv[k + 1]
