9f7b614beff58127a6f05b09c1e990bfe764a8a6
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-13 22:19:36 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
 
 if _name_ == "_main_":
+    import hidden_4
+
     noms = dir(hidden_4)
-    for nom in noms:
-        if nom[:2] != "__":
-            print(nom)
+    for name in noms:
+        if name[:2] != "__":
+            print(name)
