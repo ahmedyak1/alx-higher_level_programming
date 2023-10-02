c74d7c5a9eeaa3f8ae83c9b4596c3163131ea4c7
ahmedyak1 <ahmedyakine1@gmail.com>
2023-10-02 22:11:00 +0000

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+"""Class rectangle"""
+
+
+class Rectangle:
+    """
+    Class rectangle
+
+    Attributes:
+        width : width rectangle.
+        height : height rectangle.
+    """
+    def __init__(self, width=0, height=0):
+        """Class intance rectangle.
+
+        Args:
+            width : width rectangle.
+			height : height rectangle.
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """getter width
+
+        Return:
+            the width rectangle
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """getter height
+
+        Returns: height rectangle
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """setter width rectangle.
+
+        Args: width rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setter height recyangle
+
+        Args: height rectangle.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+			
+	def area(self):
+        """area rectangle
+
+        Returns:area
+        """
+        return  self.__width * self.__height
+
+    def perimeter(self):
+        """perimeter rectangle
+
+        Returns: perimeter
+        """
+        if  self.width == 0 or self.__height == 0 :
+            return 0
+        else:
+            return  (self.__height + self.__width) * 2
+			
+	def __str__(self):
+        """rectangle character 
+
+        Returns: the rectangle
+        """
+        recta = []
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        for k in range(self.__height):
+            for m in range(self.__width):
+                recta.append("#")
+            recta.append("\n")
+
+        recta.pop()
+
+        return "".join(recta)
+
