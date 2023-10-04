6e3d4f9d76edc72c6a94f97c8b07397e777d0ad3
ahmedyak1 <ahmedyakine1@gmail.com>
2023-10-04 22:23:27 +0000

    task update every last

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def magic_string(gtr=[]):
+    gtr += ["BestSchool"]
+    return ", ".join(gtr)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Locked class"""
+
+
+class LockedClass:
+    """
+   Locked Class create by user dynamically new astance attrivbues
+
+ 
+    """
+
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """instacen Locked class"""
+
+        self.first_name = "first_name"
+
