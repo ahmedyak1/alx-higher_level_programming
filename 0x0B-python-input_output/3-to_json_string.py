03b1ce57c004f3a2531888ff6643d2ad8de32025
ahmedyak1 <ahmedyakine1@gmail.com>
2023-10-10 23:33:53 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function to_json String"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return json presentauon 
+
+    Args: my_onj object 
+
+    Return : json file 
+    """
+    return json.dumps(my_obj)
+
