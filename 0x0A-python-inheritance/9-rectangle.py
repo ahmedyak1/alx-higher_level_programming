a29c1b54e46d8b445fe25a39ab9e0e054abccdd2
ahmedyak1 <ahmedyakine1@gmail.com>
2023-10-11 23:14:41 +0000

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Class Rectangle
+"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
@@ -8,17 +10,28 @@ class Rectangle(BaseGeometry):
     """Class Rectangle"""
 
     def __init__(self, width, height):
-        
+       """Instance rectangle
+
+        Args:
+            width width rectangle
+            height height rectangle
+        """
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
     def area(self):
-    
+        """Area Rectangle
+
+        Returns: area
+        """
         return self.__width * self.__height
 
     def __str__(self):
-       
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        """string representation Rectangle.
+
+        Return:String representation rectangle
+        """
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
 
