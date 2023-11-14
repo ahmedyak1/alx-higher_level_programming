698d6daa4654571046ce3fd841ec2f4ea5f2b2e3
ahmedyak1 <ahmedyakine1@gmail.com>
2023-11-14 23:29:59 +0000

    task 18

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+
+-- avg temperature
+-- script SQL that shows average temperature (F) by city ordered by temperature (desc)
+
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
+
