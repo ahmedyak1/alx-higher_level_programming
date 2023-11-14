befe283638b3d3ff263027b507e9669858ec833f
ahmedyak1 <ahmedyakine1@gmail.com>
2023-11-14 23:27:47 +0000

    task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- script SQL 15
+-- script SQL that lists the number of records with the same score in the table second_table 
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
+
