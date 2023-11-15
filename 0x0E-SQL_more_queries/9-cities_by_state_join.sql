1ddb3b410840fe5715a7a44f2b31e4981be3745d
ahmedyak1 <ahmedyakine1@gmail.com>
2023-11-15 23:48:55 +0000

    task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+--  Cities_by_state_join
+-- Script SQL that lists all cities contained in database hbtn_0d_usa
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id
+
