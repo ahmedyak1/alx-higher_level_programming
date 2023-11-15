2c09e95ad427ceccd288d1bf25d98f0d8281caff
ahmedyak1 <ahmedyakine1@gmail.com>
2023-11-15 23:46:54 +0000

    task 5-6

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -5,16 +5,3 @@ CREATE TABLE IF NOT EXISTS unique_id (
     name VARCHAR(256)
 );
 
-6-states.sql
-
--- States
--- Script SQL that creates the database hbtn_0d_usa and the table states
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-
