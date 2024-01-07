5e773cf20b9ab7a814e26bbb9b36a42bdc6d2434
ahmedyak1 <ahmedyakine1@gmail.com>
2024-01-07 23:59:31 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, show request and send request
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    v = response.headers.get("X-Request-Id")
+    print(v)
